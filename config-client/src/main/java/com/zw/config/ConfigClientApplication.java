49d14084f3a391db96ebb28e9cafe843f7ea0034
zouwei <zouwei@vip.qq.com>
2019-01-18 15:38:40 +0800

    测试config配置

--- config-client/src/main/java/com/zw/config/ConfigClientApplication.java
+++ config-client/src/main/java/com/zw/config/ConfigClientApplication.java
@@ -1,17 +1,23 @@
 package com.zw.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
-
 @RestController
 public class ConfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
 
-
+    @Value("{spring.application.name}")
+    private String name;
+    @GetMapping("/hi")
+    public String test() {
+        return name;
+    }
 }
