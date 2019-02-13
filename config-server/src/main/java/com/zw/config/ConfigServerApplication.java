1438274af515d11cb26c78cac858d1e8709a3f6a
zouwei <zouwei@vip.qq.com>
2019-02-13 18:50:46 +0800

    添加消息总栈

--- config-server/src/main/java/com/zw/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/zw/config/ConfigServerApplication.java
@@ -3,13 +3,11 @@ package com.zw.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
 @RestController
 public class ConfigServerApplication {
     public static void main(String[] args) {
