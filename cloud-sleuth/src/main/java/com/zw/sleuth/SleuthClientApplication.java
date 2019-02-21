b1b56693f58b76fa1782df4778c1c02c27055f3d
zouwei <zouwei@vip.qq.com>
2019-02-21 15:44:45 +0800

    添加服务依赖的链路追踪

--- cloud-sleuth/src/main/java/com/zw/sleuth/SleuthClientApplication.java
+++ cloud-sleuth/src/main/java/com/zw/sleuth/SleuthClientApplication.java
@@ -1,10 +1,8 @@
 package com.zw.sleuth;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,31 +13,11 @@ public class SleuthClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(SleuthClientApplication.class, args);
     }
-
-    @Autowired
-    private RestTemplate restTemplate;
-
     @Bean
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
 
 
-    @RequestMapping("/9100/hi")
-    public String testService() {
-        return restTemplate.getForObject("http://localhost:9101/hi/value",String.class);
-    }
-    @RequestMapping("/9100/hi/value")
-    public String testService1() {
-        return "9100";
-    }
-    @RequestMapping("/9101/hi/value")
-    public String testService2() {
-        return "9101";
-    }
-    @RequestMapping("/9101/hi")
-    public String testService3() {
-      return  restTemplate.getForObject("http://localhost:9100/hi/value", String.class);
-    }
 
 }
---
+++ cloud-sleuth/src/main/java/com/zw/sleuth/controller/TestController.java
@@ -0,0 +1,30 @@
+package com.zw.sleuth.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/9100/hi")
+    public String testService() {
+        return restTemplate.getForObject("http://localhost:9101/hi/value",String.class);
+    }
+    @RequestMapping("/9100/hi/value")
+    public String testService1() {
+        return "9100";
+    }
+    @RequestMapping("/9101/hi/value")
+    public String testService2() {
+        return "9101";
+    }
+    @RequestMapping("/9101/hi")
+    public String testService3() {
+        return  restTemplate.getForObject("http://localhost:9100/hi/value", String.class);
+    }
+}
